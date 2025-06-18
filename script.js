9ee2de15ec78c1d24e5cf8c3712b446cbdc424b9
Odai Gubran <odaimansour777@gmail.com>
2025-06-18 15:22:10 +0300

    Refactor code to use IIFE modules and factory functions as per TOP guidelines

--- script.js
+++ script.js
@@ -1,18 +1,17 @@
-const gameboard = {
-    board:
-    [
+const Gameboard = (() => {
+    const board = [
         ["", "", ""],
         ["", "", ""],
         ["", "", ""]
-    ],
-    markCell: function (row, column, currentPlayer){
+    ];
+    const markCell = (row, col, marker) => {
         
-        this.board[row][column] = currentPlayer.marker;
-        console.log(this.board)
-        
-    },
-    winnningCombinations: 
-    [
+        if (board[row][col] === "") {
+            board[row][col] = marker;
+        }  
+    };
+
+    const winningCombinations = [
         // Rows
         [[0,0], [0,1], [0,2]],
         [[1,0], [1,1], [1,2]],
@@ -27,42 +26,63 @@ const gameboard = {
         [[0,0], [1,1], [2,2]],
         [[0,2], [1,1], [2,0]]
     ]
-}
+
+    const getBoard = () => board;
+    const getWinningCombinations = () => winningCombinations;
+
+    return {
+        markCell,
+        getBoard,
+        getWinningCombinations
+    }
+})();
 
 
 const Player = (name, marker) => ({name, marker})
 
-const player1 = new Player("Odai", "X");
-const player2 = new Player("Ali", "O");
+const player1 =  Player("Odai", "X");
+const player2 =  Player("Ali", "O");
 
-let currentPlayer = player1;
 
-const gameController = {
-    placeMark: function(row, column) {
-        console.log(`${currentPlayer.name}' turn`);
-        gameboard.markCell(row, column, currentPlayer)
-        this.checkWinner()
-        this.changePlayer();
-    },
-    changePlayer: function(){
-        currentPlayer = (currentPlayer == player1) ? player2 : player1;
-    },
-    checkWinner: function(){
-        
-        for(const combo of gameboard.winnningCombinations) {
-            const [row0, col0] = combo[0];
-            const [row1, col1] = combo[1];
-            const [row2, col2] = combo[2];
-            const cellA = gameboard.board[row0][col0];
-            const cellB = gameboard.board[row1][col1];
-            const cellC = gameboard.board[row2][col2];
-            if (cellA && cellA === cellB && cellB === cellC) {
-            console.log(`${currentPlayer.name} wins with ${cellA}!`);
-            return true;
+
+const gameController = (() => {
+    let currentPlayer = player1;
+    let gameOver = false;
+
+    const changePlayer = () => {
+        currentPlayer = currentPlayer === player1 ? player2 :  player1;
+    };
+
+    const checkWinner = () => {
+        const board  = Gameboard.getBoard();
+        const combos = Gameboard.getWinningCombinations();
+
+        for(const combo of combos) {
+            const [r0, c0] = combo[0];
+            const [r1, c1] = combo[1];
+            const [r2, c2] = combo[2];
+
+            const cellA = board[r0][c0];
+            const cellB = board[r1][c1];
+            const cellC = board[r2][c2];
+
+            if (cellA && cellA === cellB && cellB === cellC){
+                console.log(`${currentPlayer.name}' Win`);
+                return true;
             }
         }
-    return false;
+        return false;
+        
+    }
+    
+    const placeMark = (row, col) => {
+        console.log(`${currentPlayer.name}' turn`);
+        Gameboard.markCell(row, col, currentPlayer.marker)
+        checkWinner()
+        changePlayer();
     }
 
+    return {placeMark};
+}) ();
     
-}
+
